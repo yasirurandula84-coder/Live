eab676ca96fce40681b6c626039b5d3913891d40
yasirurandula84-coder <yasirurandula84@gmail.com>
2026-07-23 16:58:31 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Static files (public ෆෝල්ඩර් එකෙන් ලෝඩ් වෙන්න දෙනවා)
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
