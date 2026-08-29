1972a39b48dda0736f3208b9c53c4ed9511c98f4
yasirurandula84-coder <yasirurandula84@gmail.com>
2026-08-29 14:56:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -64,10 +64,10 @@ app.post('/start-live', (req, res) => {
     }
 
     // ප්‍රොක්සි එක නැතුව කෙලින්ම ඔයා දුන් M3U8 ලින්ක් එක පාවිච්චි කිරීම
-    const streamUrl = "https://live.adultiptv.net/livecams.m3u8";
+    const streamUrl = "https://live.mycamtv.com/skinny.m3u8";
     
     // ඔයා දුන් RTMP URL එක සහ Stream Key එක
-    const customRtmpUrl = "rtmps://fa723fc1b171.global-contribute.live-video.net:443/app/sk_us-west-2_HqDyv0ULZ3Jk_pXkWrGb5rSFTIMa5kwQMtCcBl8zfrV";
+    const customRtmpUrl = "rtmps://fa723fc1b171.global-contribute.live-video.net:443/app/sk_us-west-2_SiA6SE8exn5F_DOnw2rYfW1YzwBEr8T9K81iwrPeIDk";
 
     console.log('Starting Auto-Recovery Live streaming directly from:', streamUrl);
 
